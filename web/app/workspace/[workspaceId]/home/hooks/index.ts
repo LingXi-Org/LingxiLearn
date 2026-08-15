4de0f667ed81c7dd7633c3b1e7a580e197df38d5
leecyang <3439550021@qq.com>
2026-08-15 22:41:45 +0800

    Restore workflow compatibility exports

--- web/app/workspace/[workspaceId]/home/hooks/index.ts
+++ web/app/workspace/[workspaceId]/home/hooks/index.ts
@@ -1,3 +1,7 @@
 export { createLingxiGraphAdapter } from '@/lib/lingxi/lingxi-graph-adapter'
-export { getLingxiGraphUseChatOptions, useChat } from './use-chat'
+export {
+  getLingxiGraphUseChatOptions,
+  getLingxiGraphUseChatOptions as getWorkflowCopilotUseChatOptions,
+  useChat,
+} from './use-chat'
 export { useMothershipResize } from './use-mothership-resize'
--- web/ee/access-control/utils/permission-check.ts
+++ web/ee/access-control/utils/permission-check.ts
@@ -14,7 +14,15 @@ export function validateInvitationsAllowed(..._args: any[]) { return true }
 export function validateMcpToolsAllowed(..._args: any[]) { return true }
 export function validateCustomToolsAllowed(..._args: any[]) { return true }
 export function validateSkillsAllowed(..._args: any[]) { return true }
+export function validatePublicApiAllowed(..._args: any[]) { return true }
+export function validatePublicFileSharing(..._args: any[]) { return true }
+export function validateChatDeployAuth(..._args: any[]) { return true }
 export function assertPermissionsAllowed(..._args: any[]) { return true }
+
+export class InvitationsNotAllowedError extends Error {}
+export class PublicApiNotAllowedError extends Error {}
+export class PublicFileSharingNotAllowedError extends Error {}
+export class ChatDeployAuthNotAllowedError extends Error {}
 
 export class ToolNotAllowedError extends Error {
   constructor(message = 'Tool is not allowed') {
